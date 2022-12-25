8d6d207ee95c7246844f1ff43031cc4c79de87aa
malak-Alkindi <120641708+malak-Alkindi@users.noreply.github.com>
2022-12-25 21:06:12 +0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig :{
-    apiKey: "AIzaSyC_Sw0oEXCNNztmf9CGwhzGsVlhV67ssWI",
-    authDomain: "isfreeroom.firebaseapp.com",
-    databaseURL: "https://isfreeroom-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "isfreeroom",
-    storageBucket: "isfreeroom.appspot.com",
-    messagingSenderId: "574972255228",
-    appId: "1:574972255228:web:ee4b2d0f7c9e193daa7079",
-    measurementId: "G-JJPJ286P87"
+    apiKey: "******",
+    authDomain: "*********.com",
+    databaseURL: "**************",
+    projectId: "*****8",
+    storageBucket: "*****.appspot.com",
+    messagingSenderId: "****",
+    appId: "*********",
+    measurementId: "********"
   },
 };
 
